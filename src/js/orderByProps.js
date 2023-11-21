e02dc11d9aa41914d1d13c5011326d2be7a39a07
Daria <n-d-p@mail.ru>
2023-11-21 20:15:52 +0300

    to clean code

--- src/js/orderByProps.js
+++ src/js/orderByProps.js
@@ -20,15 +20,6 @@ export default function orderByProps(objForSorted, arrForOrder) {
     return result;
 }
 
-const obj = {health: 10, level: 2, attack: 80, defence: 40, name: 'мечник'}
-console.log(orderByProps(obj, ["name", "level"]))
-/*[
-  {key: "name", value: "мечник"}, // порядок взят из массива с ключами
-  {key: "level", value: 2}, // порядок взят из массива с ключами
-  {key: "attack", value: 80}, // порядок по алфавиту (т.к. в массиве с ключами нет значения "attack")
-  {key: "defence", value: 40}, // порядок по алфавиту (т.к. в массиве с ключами нет значения "defence")
-  {key: "health", value: 10} // порядок по алфавиту (т.к. в массиве с ключами нет значения "health")
-]*/
 
 
 
